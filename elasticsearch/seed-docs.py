a4a9945b317cdc1f1f7c18d74b7bc88676784308
Espinal, Fausto J <Fausto.Espinal@med.ge.com>
2017-10-20 15:44:58 -0500

    Declare an explicit JSON schema to store geographical locations.

--- elasticsearch/seed-docs.py
+++ elasticsearch/seed-docs.py
@@ -14,8 +14,45 @@ es = Elasticsearch(
     use_ssl=False
 )
 
+institutions = ['Sports Institute', 'Research Institute', 'FC Institute', 'ABC Institute', 'XYZ Institute']
+locations = ['40.7128, 74.0060', '34.0522, 118.2437', '41.8781, 87.6298', '48.8566, 2.3522', '39.9042, 116.4074']
+
+indexDef = {
+  'mappings': {
+      'entry-type' : {
+          'properties' : {
+                'id' : {
+                    'type' : 'integer'
+                },
+                'timestamp' : {
+                    'type' : 'date'
+                },
+                'institution' : {
+                    'type' : 'string',
+                    'analyzer' : 'keyword'
+                },
+                'author' : {
+                    'type' : 'string'
+                },
+                'tags' : {
+                    'type' : 'string'
+                },
+                'content' : {
+                    'type' : 'text'
+                },
+                'location' : {
+                    'type' : 'geo_point'
+                }
+          }
+      }
+  }
+}
+index_name = 'rad-data-index'
+es.indices.create(index=index_name,body=indexDef)
+
 idCounter = 101
 contentPath = sys.argv[1]
+idx = 0
 for root, directories, filenames in os.walk(contentPath):
     # for directory in directories:
     #     print(os.path.join(root, directory) )
@@ -28,29 +65,25 @@ for root, directories, filenames in os.walk(contentPath):
                 myfile.close()
             
             words = data.split(' ')
-            drName = 'Unknown'
-            for wIdx in range(0,len(words)):
-                w = words[wIdx]
-                if w.lower().startswith('dr.'):
-                    drName = w + ' ' + words[wIdx+1]
-                    print('DrName=',drName)
+            author = 'Author unknown'
 
             doc = {
-                'author': drName,
-                'text': data,
+                'author': author,
+                'content': data,
                 'timestamp': datetime.now(),
+                'institution': institutions[idx],
+                'location' : locations[idx],
+                'tags' : 'radiology, open-source'
             }
-            res = es.index(index="clinical-data-index", doc_type='radiology-report', id=idCounter, body=doc)
-            # res = {
-            #     'created' : False
-            # }
+            idx = (idx+1) % len(locations)
+            res = es.index(index=index_name, doc_type='rad-report', id=idCounter, body=doc)
             print('Created radiology report:', res['created'])
             idCounter = idCounter + 1
             #print(data)
             print('=======================================')
 
 
-es.indices.refresh(index="clinical-data-index")
+es.indices.refresh(index=index_name)
 
 res = es.search(index="test-index", body={"query": {"match_all": {}}})
 print("Got %d Hits:" % res['hits']['total'])
